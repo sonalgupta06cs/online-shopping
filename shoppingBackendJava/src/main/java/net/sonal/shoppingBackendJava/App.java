aaa9864a152af31fb377a99d629e88e47e021283
SonalGupta <sonalgupta06cs@gmail.com>
2018-05-14 09:53:57 +0530

    My Project Setup completed successfully..

---
+++ shoppingBackendJava/src/main/java/net/sonal/shoppingBackendJava/App.java
@@ -0,0 +1,13 @@
+package net.sonal.shoppingBackendJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
