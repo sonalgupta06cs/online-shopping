1d1d345a9b423bf019993e1dc84b27c7b800c793
SonalGupta <sonalgupta06cs@gmail.com>
2018-05-15 11:33:11 +0530

    Second Commit..

---
+++ onlineshoppingstore/src/main/java/net/sonal/onlineshopping/controller/PageController.java
@@ -0,0 +1,18 @@
+package net.sonal.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		 
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("greeting", "Welcome to Spring MVC");
+		return modelAndView;
+	}
+	
+}
